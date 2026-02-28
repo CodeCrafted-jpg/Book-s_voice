c9c88217483c32b46c81cacf76e4b5033a40d63b
Sayan Mallick <malsayan6@gmail.com>
2026-02-28 12:34:58 +0530

    setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <header className='w-full fixed z-50 bg-(--bg-primary)'>
+ <div className='wrapper navbar-height py-4 flex
+    justify-between items-center'>
+      <Link href={'/'} className='flex gap-0.5 items-center'>
+      <Image src={'/assets/logo.png'}  alt='logo' height={42} width={38}/>
+      </Link>
     </div>
+    </header>
+   
   )
 }
 
