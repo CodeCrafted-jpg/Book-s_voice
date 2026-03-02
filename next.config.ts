0f74be02263e8266089e802eb827a0a7fa73ccce
Sayan Mallick <malsayan6@gmail.com>
2026-03-02 15:50:49 +0530

    vercel storage setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-images: {remotePatterns: [{protocol: "https", hostname: "covers.openlibrary.org", }]},
+images: {remotePatterns: [
+    {protocol: "https", hostname: "covers.openlibrary.org", },
+     {protocol: "https", hostname: "xuwjfwsu6pm2xocq.public.blob.vercel-storage.com", }
+]},
 };
 
 export default nextConfig;
